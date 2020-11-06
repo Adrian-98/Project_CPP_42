9ecc1d779b1036f641ff5ba01b61036862d13352
Adrian Munoz perez <amunoz-p@c3r12s6.42madrid.com>
2020-11-06 18:45:53 +0100

    ex00 done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amunoz-p <amunoz-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/06 17:29:09 by amunoz-p          #+#    #+#             */
+/*   Updated: 2020/11/06 18:44:52 by amunoz-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <locale>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string str(argv[i]);
+			for (size_t j = 0; j < str.length(); j++)
+				std::cout << (char)std::toupper(str[j]);
+			std::cout << " ";
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}
