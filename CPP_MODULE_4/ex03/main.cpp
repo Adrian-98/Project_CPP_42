672677a355a62477e208b235a933c49f0796da3e
Adrian Munoz perez <amunoz-p@c1r15s3.42madrid.com>
2020-12-02 18:20:24 +0100

    ex03 module 4 started

---
+++ CPP_MODULE_4/ex03/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amunoz-p <amunoz-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/02 18:16:50 by amunoz-p          #+#    #+#             */
+/*   Updated: 2020/12/02 18:18:59 by amunoz-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main()
+{
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+
+	ICharacter* me = new Character("me");
+	
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	
+	ICharacter* bob = new Character("bob");
+	
+	me->use(0, *bob);
+	me->use(1, *bob);
+	
+	delete bob;
+	delete me;
+	delete src;
+	
+	return 0;
+}
