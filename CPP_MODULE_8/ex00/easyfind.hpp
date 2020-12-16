56c86bf2df0af22653c36794488204ec413e05d1
Adrian Munoz perez <amunoz-p@c1r15s5.42madrid.com>
2020-12-16 20:16:47 +0100

    ex01 module 8 almost finished

--- CPP_MODULE_8/ex00/easyfind.hpp
+++ CPP_MODULE_8/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: amunoz-p <amunoz-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/15 19:28:52 by amunoz-p          #+#    #+#             */
-/*   Updated: 2020/12/16 18:58:28 by amunoz-p         ###   ########.fr       */
+/*   Updated: 2020/12/16 19:40:41 by amunoz-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,26 @@
 #include <iostream>
 #include <vector>
 #include <string.h>
+#include <exception>
 
 template<typename T>
 void easyfind(T &a, int n)
 {
     typename T::iterator found;
     found = std::find(a.begin(), a.end(), n);
-    if (found != a.end())
-        std::cout << "Value has been found!\n---------\n";
-    else 
-        std::cout << "Value has not been found!\n--------\n";
+    try
+    { 
+        if (found != a.end())
+            std::cout << "Value has been found!\n---------\n";
+        else 
+        {
+            throw std::exception(); 
+        }
+    }
+    catch (std::exception e)
+    {
+        std::cout << "Value has NOT been found!\n--------\n";
+    }
 }
 
 
